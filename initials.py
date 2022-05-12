1c7d26036d641d1c97f02997330cc330c096cd17
ryan-charette <ryanacharette@gmail.com>
2022-05-12 03:04:53 -0500

    Create initials.py

---
+++ initials.py
@@ -0,0 +1,19 @@
+#Write a Python program called initials.py that displays the initials of your name in block letters.
+#Each block letter should be 7x5 and formed by the letter itself.
+#Write two comments with:
+    #Your first and last name.
+    #A fun fact about yourself.
+
+#Ryan Charette
+#Fun Fact: I volunteered at a puppet museum one summer
+
+initials = """
+RRRR   CCC
+R   R C   C
+R   R C
+RRRR  C
+R R   C
+R  R  C   C
+R   R  CCC
+"""
+print(initials)
