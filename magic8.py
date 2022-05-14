bc7eca7d09520adc421e5bbeedb97c5f40654f93
ryan-charette <ryanacharette@gmail.com>
2022-05-14 01:00:44 -0500

    Update magic8.py
    - Added error catching
    - Added more possible answers

--- magic8.py
+++ magic8.py
@@ -1,11 +1,16 @@
 import random
+import sys
 
-name = input("What is your name?")
+name = input("What is your name? ")
 question = input('Ask a "Yes" or "No" question about the future: ')
-random_number = random.randint(0, 8)
-answers = ["Yes - definitely.", "It is decidedly so.", "Without a doubt", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "My sources say no.", "Outlook not so good.", "Very doubtful."]
+random_number = random.randint(0, 19)
+answers = ["It is certain.", "It is decidedly so.", "Without a doubt.", "Yes - definitely.", "You may rely on it.", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."]
 
-
-
-print(name + " asks: " + question)
+if question == "":
+    print("It is forbidden to provide a fortune to those who do not ask a question.")
+    sys.exit()
+elif name == "":
+    print("Question: " + question)
+else:
+    print(name + " asks: " + question)
 print("Magic 8-Ball's answer: " + answers[random_number])
