8f3763e49f0936c86b4afa64d712da4064a804c6
ryan-charette <ryanacharette@gmail.com>
2022-05-20 14:17:41 -0500

    Update code_challenges_1.py
    - Completed basic and advanced code challenges for loops

--- code_challenges_1.py
+++ code_challenges_1.py
@@ -205,3 +205,115 @@ def middle_element(lst):
         return lst[len(lst) // 2]
     else:
         return (lst[len(lst) // 2] + lst[len(lst) // 2 - 1]) / 2
+
+#Python Code Challenges involving loops
+
+#1. Divisible by Ten
+#Create a function named divisible_by_ten() that takes a list of numbers named nums as a parameter.
+#Return the count of how many numbers in the list are divisible by 10.
+
+def divisible_by_ten(nums):
+    counter = 0
+    for num in nums:
+        if num % 10 == 0:
+            counter += 1
+    return counter
+
+#2. Greetings
+#Create a function named add_greetings() which takes a list of strings named names as a parameter.
+#In the function, create an empty list that will contain each greeting. Add the string 'Hello, ' in front of each name in names and append the greeting to the list.
+#Return the new list containing the greetings.
+
+def add_greetings(names):
+    greetings = []
+    for name in names:
+        greetings.append("Hello, " + name)
+    return greetings
+
+#3. Delete Starting Even Numbers
+#Write a function called delete_starting_evens() that has a parameter named lst.
+#The function should remove elements from the front of lst until the front of the list is not even. The function should then return lst.
+#For example if lst started as [4, 8, 10, 11, 12, 15], then delete_starting_evens(lst) should return [11, 12, 15].
+#Make sure your function works even if every element in the list is even!
+
+def delete_starting_evens(lst):
+    while len(lst) > 0 and lst[0] % 2 == 0:
+        lst.pop(0)
+    return lst
+
+#4. Odd Indicies
+#Create a function named odd_indices() that has one parameter named lst.
+#The function should create a new empty list and add every element from lst that has an odd index. The function should then return this new list.
+#For example, odd_indices([4, 3, 7, 10, 11, -2]) should return the list [3, 10, -2].
+
+def odd_indices(lst):
+    lst_odds = [lst[i] for i in range(1, len(lst), 2)]
+    return lst_odds
+
+#5. Exponents
+#Create a function named exponents() that takes two lists as parameters named bases and powers. Return a new list containing every number in bases raised to every number in powers.
+#For example, consider the following code.
+#exponents([2, 3, 4], [1, 2, 3])
+#the result would be the list [2, 4, 8, 3, 9, 27, 4, 16, 64]. It would first add two to the first. Then two to the second. Then two to the third, and so on.
+
+def exponents(bases, powers):
+    exponent_lst = []
+    for base in bases:
+        for power in powers:
+            exponent_lst.append(base ** power)
+    return exponent_lst
+
+#Difficult Python Code Challenges involving Loops
+
+#1. Larger Sum
+#Create a function named larger_sum() that takes two lists of numbers as parameters named lst1 and lst2.
+#The function should return the list whose elements sum to the greater number. If the sum of the elements of each list are equal, return lst1.
+
+def larger_sum(lst1, lst2):
+    if sum(lst1) >= sum(lst2):
+        return lst1
+    else:
+        return lst2
+
+#2. Over 9000
+#Create a function named over_nine_thousand() that takes a list of numbers named lst as a parameter.
+#The function should sum the elements of the list until the sum is greater than 9000. When this happens, the function should return the sum. If the sum of all of the elements is never greater than 9000, the function should return total sum of all the elements. If the list is empty, the function should return 0.
+#For example, if lst was [8000, 900, 120, 5000], then the function should return 9020.
+
+def over_nine_thousand(lst):
+    sum = 0
+    for num in lst:
+        sum += num
+        if sum > 9000:
+            break
+    return sum
+
+#3. Max Num
+#Create a function named max_num() that takes a list of numbers named nums as a parameter.
+#The function should return the largest number in nums
+
+def max_num(nums):
+    return max(nums)
+
+#4. Same Values
+#Write a function named same_values() that takes two lists of numbers of equal size as parameters.
+#The function should return a list of the indices where the values were equal in lst1 and lst2.
+#For example, the following code should return [0, 2, 3]
+#same_values([5, 1, -10, 3, 3], [5, 10, -10, 3, 5])
+
+def same_values(lst1, lst2):
+    equal_lst = [i for i in range(len(lst1)) if lst1[i] == lst2[i]]
+    return equal_lst
+
+#5. Reversed List
+#Create a function named reversed_list() that takes two lists of the same size as parameters named lst1 and lst2.
+#The function should return True if lst1 is the same as lst2 reversed. The function should return False otherwise.
+#For example, reversed_list([1, 2, 3], [3, 2, 1]) should return True.
+
+def reversed_list(lst1, lst2):
+    if lst1 == list(reversed(lst2)):
+        return True
+    else:
+        return False
+
+#Python Code Challenges involving functions
