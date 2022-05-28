28b039a6ff42607ebe1b3226a7ee1b72a2096e9e
ryan-charette <100053398+ryan-charette@users.noreply.github.com>
2022-05-28 00:47:18 -0500

    Add interest finder logic

--- midterm_project.py
+++ midterm_project.py
@@ -31,3 +31,20 @@ add_attraction("São Paulo, Brazil", ["São Paulo Zoo", ["zoo"]])
 add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]])
 add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
 add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
+
+def find_attractions(destination, interests):
+  destination_index = get_destination_index(destination)
+  attractions_in_city = attractions[destination_index]
+  attractions_with_interest = []
+  attraction_tags = []
+  for possible_attraction in attractions_in_city:
+    for tag in possible_attraction[1]:
+      attraction_tags.append(tag)
+    for interest in interests:
+      if interest in attraction_tags:
+        attractions_with_interest.append(possible_attraction[0])
+  return attractions_with_interest
+      
+test_la_arts = find_attractions("Los Angeles, USA", ["art"])
+print(test_la_arts)
+  
