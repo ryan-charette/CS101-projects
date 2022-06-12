db68120b0a11d9b3b685e96cbf8841eb5fa0b31c
ryan-charette <100053398+ryan-charette@users.noreply.github.com>
2022-06-11 23:26:49 -0500

    Add difficult string challenges

--- code_challenges/set2.py
+++ code_challenges/set2.py
@@ -59,3 +59,57 @@ def x_length_words(sentence, x):
         if len(word) < x:
             return False
     return True
+
+# Difficult Python Code Challenges Involving Strings
+
+# 1. Check Name
+
+# Write a function called check_for_name that takes two strings as parameters named sentence and name. 
+# The function should return True if name appears in sentence in all lowercase letters, all uppercase letters, or with any mix of uppercase and lowercase letters. 
+# The function should return False otherwise.
+# For example, the following three calls should all return True:
+# check_for_name("My name is Jamie", "Jamie")
+# check_for_name("My name is jamie", "Jamie")
+# check_for_name("My name is JAMIE", "Jamie")
+
+def check_for_name(sentence, name):
+    return name.lower() in sentence.lower()
+
+# 2. Every Other Letter
+
+# Create a function named every_other_letter that takes a string named word as a parameter. 
+# The function should return a string containing every other letter in word.
+
+def every_other_letter(word):
+    even_index_letters = ""
+    for i in range(0, len(word), 2):
+        even_index_letters += word[i]
+    return even_index_letters
+
+# 3. Reverse
+# Write a function named reverse_string that has a string named word as a parameter. 
+# The function should return word in reverse.
+
+def reverse_string(word):
+    return "".join(reversed(word))
+
+# 4. Make Spoonerism
+# Write a function called make_spoonerism that takes two strings as parameters named word1 and word2. 
+# Finding the first syllable of a word is a difficult task, so for our function, we’re going to switch the first letters of each word. 
+# Return the two new words as a single string separated by a space.
+
+def make_spoonerism(word1, word2):
+    word1_swapped = word2[0] + word1[1:]
+    word2_swapped = word1[0] + word2[1:]
+    return "{} {}".format(word1_swapped, word2_swapped)
+
+# 5. Add Exclamation
+# Create a function named add_exclamation that has one parameter named word. 
+# This function should add exclamation points to the end of word until word is 20 characters long. 
+# If word is already at least 20 characters long, just return word.
+
+def add_exclamation(word):
+    exclam = word
+    if len(exclam) < 20:
+        exclam += "!" * (20 - len(exclam))
+    return exclam
